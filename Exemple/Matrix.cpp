c2dce6565d3556c06a8f2685d3f0b672eaa68277
Braunschweig Malo <malo.braunschweig@live.fr>
2022-07-19 11:58:18 +0200

    :pencil2: SquaRe

--- Exemple/Matrix.cpp
+++ Exemple/Matrix.cpp
@@ -5,7 +5,7 @@ PikControlMatrix8x8 matrix(4);
 
 void setup(){
   matrix.Clear();
-  matrix.GenerateSquarre(1 ,1,6,250,10,10);
+  matrix.GenerateSquare(1 ,1,6,250,10,10);
   for(int i =0; i<8; i++){
     matrix.SetPixelByCoord(i,i,0,250,0);
     matrix.SetPixelByCoord(7-i,i,0,250,0);
--- PikControlMatrixLib.h
+++ PikControlMatrixLib.h
@@ -10,7 +10,7 @@ class PikControlMatrix8x8
         Adafruit_NeoPixel *pixels;
         PikControlMatrix8x8(int _pin);
         int Convert(int x,int y);
-        void GenerateSquarre(int x, int y, int taille, int r,int g,int b);
+        void GenerateSquare(int x, int y, int taille, int r,int g,int b);
         void SetPixelByCoord(int x, int y, int r,int g,int b);
         void Clear();
         int pin;
